f9ab02ac7730a3cc16773a666c71d497005dd266
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-10-20 12:02:44 +0200

    test add origin

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express()
 
 /* Configuration du header */
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000/')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
   next()
