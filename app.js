081f6a21ea320a408a856784c09916b2607846de
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-10-20 11:54:40 +0200

    active set header

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const share = require('./routes/share')
 const app = express()
 
 /* Configuration du header */
-/* app.use((req, res, next) => {
+app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
   next()
-})   */  
+})    
 
 app.use(express.json())
 app.use(cors())
