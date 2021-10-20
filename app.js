0bf6b861dd47b1a01eb6ce8274151bd01259232d
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-10-20 11:50:13 +0200

    install cors

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const cors = require('cors')
 const users = require('./routes/user')
 const feelings = require('./routes/feeling')
 const share = require('./routes/share')
@@ -7,14 +7,15 @@ const share = require('./routes/share')
 const app = express()
 
 /* Configuration du header */
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*') /* * pour la soutenance, pour la presentation mettre le 8080 */
+/* app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
   next()
-})    
+})   */  
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => res.send('Success!!!'));
 app.use('/api/', users)
