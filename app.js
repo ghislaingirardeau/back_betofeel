84404d6222bb9aacbd0ae331ce75a3e0cf6e6285
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-10-21 14:08:08 +0200

    comment set header & define cors origin

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const corsOptions ={
       callback(new Error('Not allowed by CORS'))
     }
   }, */
-  origin: "*",
+  origin: "https://betofeel.netlify.app",
   credentials:true,            
   optionSuccessStatus:200,
 }
@@ -25,7 +25,7 @@ app.use(cors(corsOptions))
 
 /* Configuration du header */
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://betofeel.netlify.app') /* * pour la soutenance, pour la presentation mettre le 8080 */
+  /* res.setHeader('Access-Control-Allow-Origin', 'https://betofeel.netlify.app')  */
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
   next()
