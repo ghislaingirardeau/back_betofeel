327d06039fc5c95f5a2bce49c258867ca38a1edb
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-10-21 12:59:40 +0200

    test

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cors(corsOptions)) */
 
 /* Configuration du header */
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*') /* * pour la soutenance, pour la presentation mettre le 8080 */
+  res.setHeader('Access-Control-Allow-Origin', 'https://betofeel.netlify.app') /* * pour la soutenance, pour la presentation mettre le 8080 */
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
   next()
