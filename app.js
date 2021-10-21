c0d7664b6459eab572bcd783221837762316bec6
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-10-21 12:20:35 +0200

    origin from netlify only

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ const app = express()
 var whitelist = ['http://localhost:8000', 'https://betofeel.netlify.app']
 
 const corsOptions ={
-  origin: function (origin, callback) {
+/*   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
-      callback(new Error('Not allowed CORS'))
+      callback(new Error('Not allowed by CORS'))
     }
-  },
+  }, */
+  origin: "https://betofeel.netlify.app",
   credentials:true,            
   optionSuccessStatus:200,
 }
