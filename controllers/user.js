a9d19c2b70ff2beeac6b5982cdcaed5eb898e5b1
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-10-21 15:02:52 +0200

    test handle crash app

--- controllers/user.js
+++ controllers/user.js
@@ -4,14 +4,40 @@ const jwt = require('jsonwebtoken')
 /* CONNECTION MYSQL */
 const mysql = require('mysql');
 const config = require('../config');
-const connection = mysql.createConnection(config);
+/* const connection = mysql.createConnection(config); */
 
-connection.connect(function(err) {
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }
+    console.log('Connected to the MySQL server');                                    
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
+/* connection.connect(function(err) {
     if (err) {
       return console.error('error: ' + err.message);
     }
 console.log('Connected to the MySQL server');
-});
+}); */
 
 const salt = 10
 
