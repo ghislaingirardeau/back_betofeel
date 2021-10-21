1e41d8a90834a13a2cf4ff746255f55d74039530
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-10-21 12:35:14 +0200

    add slash

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cors(corsOptions)) */
 
 /* Configuration du header */
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://betofeel.netlify.app')
+  res.setHeader('Access-Control-Allow-Origin', 'https://betofeel.netlify.app/')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
   next()
