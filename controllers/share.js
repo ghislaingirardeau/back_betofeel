a349f9f3dbd6b757891e3ffd1ef300713ab6088e
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-10-21 16:40:32 +0200

    add create connection

--- controllers/share.js
+++ controllers/share.js
@@ -1,32 +1,7 @@
 /* CONNECTION MYSQL */
 const mysql = require('mysql')
 const config = require('../config')
-
-// DETAIL SUR CONTROLLER USER
-var connection;
-
-function handleDisconnect() {
-  connection = mysql.createConnection(config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
-
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }
-    console.log('Connected to the MySQL server');                                    
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-}
-handleDisconnect();
+const connection = mysql.createConnection(config);
 
 exports.searchUser = (req, res, next) => {
     
