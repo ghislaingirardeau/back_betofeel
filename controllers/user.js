11d06406f83247af9a2ea3e5a9fa6ff06168bd62
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-10-22 12:09:18 +0200

    signup add async DB connection

--- controllers/user.js
+++ controllers/user.js
@@ -13,66 +13,73 @@ function closeDb (connection) {
         }
         console.log('Close the database connection.');
     });
-
 }
 
 const salt = 10
 
 /* signup */
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
 
-    const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-    const regexPassword = /^.*(?=.{6,})(?=.*\d)(?=.*[a-zA-Z]).*$/ /* Minimum 6 caracteres dont 1 lettre et une chiffre */
-    const pseudoLowCase = req.body.pseudo.toLowerCase()
-    if (regexEmail.test(req.body.email) === true && regexPassword.test(req.body.password) === true) { /* verifie la saiise des entrées */
-        
-        bcrypt.hash(req.body.password, salt)
-        .then(hash => {
-
-            const sql = `SET @pseudo="${pseudoLowCase}", @email=AES_ENCRYPT('${req.body.email}','clesecrete'), @password="${hash}", @avatar="${req.body.avatar}";`
-            connection.query(sql, (error, results, fields) => {
-                
-                if (error) {
-                    res.status(400).json({message: 'erreur de variable'})
-                }
-                else if(results){
+    const connection = await mysql.createConnection(config)
+    if(connection) {
+        const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+        const regexPassword = /^.*(?=.{6,})(?=.*\d)(?=.*[a-zA-Z]).*$/ /* Minimum 6 caracteres dont 1 lettre et une chiffre */
+        const pseudoLowCase = req.body.pseudo.toLowerCase()
+        if (regexEmail.test(req.body.email) === true && regexPassword.test(req.body.password) === true) { /* verifie la saiise des entrées */
+            
+            bcrypt.hash(req.body.password, salt)
+            .then(hash => {
+    
+                const sql = `SET @pseudo="${pseudoLowCase}", @email=AES_ENCRYPT('${req.body.email}','clesecrete'), @password="${hash}", @avatar="${req.body.avatar}";`
+                connection.query(sql, (error, results, fields) => {
                     
-                    /* const sql = `CALL signup(@pseudo, @email, @password)`; */
-                    const sql = `INSERT INTO user (pseudo, email, password, avatar)
-                    VALUES (@pseudo, @email, @password, @avatar);`; 
-                    connection.query(sql, (error, result, fields) => {
-                        if (error) {
-                            res.status(400).json({message: "pseudo existe deja"})
-                            console.log(error)
-                        }
-                        else if(result){ 
-                            const sql = `SELECT pseudo, id, roles, avatar FROM user WHERE pseudo=@pseudo AND email=@email;`
-                            connection.query(sql, (error, result, fields) => {
-                                if (error) {
-                                    res.status(400).json({message: "erreur de selection"})
-                                }
-                                else if(result) {
-                                    res.status(200).json({
-                                        pseudo: result[0].pseudo,
-                                        userId: result[0].id,
-                                        token: jwt.sign({
-                                            userId: result[0].id}, `${process.env.CLE}`,
-                                            { expiresIn: '24h'}),
-                                        role: result[0].roles,
-                                        avatar: result[0].avatar
-                                    })
-                                }
-                            })
-                        } 
-                    });
-                }
-            });
-        })
-        .catch(() => res.status(400).json({message: 'Echec'}))
-    } else {
-        res.status(401).json({message: 'Le format de donnée ne correspond pas !'})
+                    if (error) {
+                        res.status(400).json({message: 'erreur de variable'})
+                        closeDb(connection)
+                    }
+                    else if(results){
+                        
+                        /* const sql = `CALL signup(@pseudo, @email, @password)`; */
+                        const sql = `INSERT INTO user (pseudo, email, password, avatar)
+                        VALUES (@pseudo, @email, @password, @avatar);`; 
+                        connection.query(sql, (error, result, fields) => {
+                            if (error) {
+                                res.status(400).json({message: "pseudo existe deja"})
+                                closeDb(connection)
+                            }
+                            else if(result){ 
+                                const sql = `SELECT pseudo, id, roles, avatar FROM user WHERE pseudo=@pseudo AND email=@email;`
+                                connection.query(sql, (error, result, fields) => {
+                                    if (error) {
+                                        res.status(400).json({message: "erreur de selection"})
+                                        closeDb(connection)
+                                    }
+                                    else if(result) {
+                                        res.status(200).json({
+                                            pseudo: result[0].pseudo,
+                                            userId: result[0].id,
+                                            token: jwt.sign({
+                                                userId: result[0].id}, `${process.env.CLE}`,
+                                                { expiresIn: '24h'}),
+                                            role: result[0].roles,
+                                            avatar: result[0].avatar
+                                        })
+                                        closeDb(connection)
+                                    }
+                                })
+                            } 
+                        });
+                    }
+                });
+            })
+            .catch(() => res.status(400).json({message: 'Echec'}))
+        } else {
+            res.status(401).json({message: 'Le format de donnée ne correspond pas !'})
+            closeDb(connection)
+        }
     }
+
 }
 
 exports.login = async (req, res, next) => { 
