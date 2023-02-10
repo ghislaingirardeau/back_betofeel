bcda27aceb5510c7257dd4ce9c1a004490cf0f53
ghislaingirardeau <g.girardeaupro@sfr.fr>
2023-02-10 15:04:42 +0100

    change origin

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
-const express = require('express')
-const cors = require('cors')
-const users = require('./routes/user')
-const feelings = require('./routes/feeling')
-const share = require('./routes/share')
+const express = require("express");
+const cors = require("cors");
+const users = require("./routes/user");
+const feelings = require("./routes/feeling");
+const share = require("./routes/share");
 
-const app = express()
+const app = express();
 
-const corsOptions ={
-  origin: ['https://betofeel.netlify.app', 'http://localhost:8000'],
-  credentials:true,            
-  optionSuccessStatus:200,
-}
+const corsOptions = {
+  origin: [
+    "https://web-production-b214.up.railway.app",
+    "http://localhost:8000",
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 /* Configuration du header */
 app.use((req, res, next) => {
   /* res.setHeader('Access-Control-Allow-Origin', 'https://betofeel.netlify.app')  */
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
-  next()
-})    
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH"
+  );
+  next();
+});
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => res.send('Success!!!'));
-app.use('/api/', users)
-app.use('/api/feeling', feelings)
-app.use('/api/share', share)
+app.get("/", (req, res) => res.send("Success!!!"));
+app.use("/api/", users);
+app.use("/api/feeling", feelings);
+app.use("/api/share", share);
 
-module.exports = app
+module.exports = app;
